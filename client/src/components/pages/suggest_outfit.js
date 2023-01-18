53904c135a792d782dd3be823edd82ddbe5e9a7e
fyanez <fabianyanez100@gmail.com>
2023-01-17 23:20:11 -0600

    not home page

---
+++ client/src/components/pages/suggest_outfit.js
@@ -0,0 +1,26 @@
+import "./suggest_outfits.css";
+
+const Suggest_Outfits_Page = () => {
+  return (
+    <>
+      <html>
+        <head>
+          <meta charset="UTF-8" />
+          <link rel="shortcut icon" href="/static/favicon.ico" type="image/x-icon" />
+          <link rel="icon" href="/static/favicon.ico" type="image/x-icon" />
+          <title>Gamebook</title>
+        </head>
+
+        <body>
+          <div>
+            <p>Test</p>
+          </div>
+          <div id="root"></div>
+          <script src="/bundle.js"></script>
+        </body>
+      </html>
+    </>
+  );
+};
+
+export default Suggest_Outfits_Page;
