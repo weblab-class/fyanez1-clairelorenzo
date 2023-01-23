26c40b30dcd9afee5ca4c421c048e5273647f16f
Claire Lorenzo <clairelorenzo@dhcp-10-29-14-215.dyn.mit.edu>
2023-01-23 13:05:54 -0500

    started mongoose schemas

--- server/models/bottom.js
+++ server/models/bottom.js
@@ -1,1 +1,11 @@
 const mongoose = require("mongoose");
+
+const BottomSchema = new mongoose.Schema({
+    item_name: String,
+    item_warmth: String,
+    item_color: String,
+    item_style: String,
+    image_ref: String,
+})
+
+module.exports=mongoose.model('bottom',BottomSchema)
--- server/models/dress.js
+++ server/models/dress.js
@@ -1,1 +1,11 @@
 const mongoose = require("mongoose");
+
+const DressSchema = new mongoose.Schema({
+    item_name: String,
+    item_warmth: String,
+    item_color: String,
+    item_style: String,
+    image_ref: String,
+})
+
+module.exports=mongoose.model('dress',DressSchema)
